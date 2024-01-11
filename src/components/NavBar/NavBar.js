e85e3bf1996b5d3484fcb5ddc5bee82ded42d581
Hugo Gouzy <110604228+gotin23@users.noreply.github.com>
2024-01-11 22:10:46 +0100

    feat: Navigation assets and tailwindconfig

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,43 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import BikeIcon from "../../assets/icons/cargo-bike-tricycle.svg";
+
+function NavBar() {
+  return (
+    <nav className="w-6/6 bg-white flex justify-between items-center p-6 shadow-md	">
+      <div className="flex">
+        <Image src={BikeIcon} className="w-12 mr-5" />
+        <h1 className="text-3xl">Les cochers solidaires</h1>
+      </div>
+      <ul className="flex text-xl">
+        <Link
+          href={"/"}
+          className="text-red relative  after:content-[''] after:absolute after:bg-red after:h-[2px] after:w-0 after:top-1/2 after:left-1/2 after:transform after:-translate-x-1/2 after:-translate-y-[-10px] hover:after:w-full hover:after:transition-all hover:after:duration-200"
+        >
+          Acceuil
+        </Link>
+        <Link
+          className="ml-4 text-red	relative  after:content-[''] after:absolute after:bg-red after:h-[2px] after:w-0 after:top-1/2 after:left-1/2 after:transform after:-translate-x-1/2 after:-translate-y-[-10px] hover:after:w-full hover:after:transition-all hover:after:duration-200"
+          href={"/"}
+        >
+          Association
+        </Link>
+        <Link
+          className="ml-4 text-red	relative  after:content-[''] after:absolute after:bg-red after:h-[2px] after:w-0 after:top-1/2 after:left-1/2 after:transform after:-translate-x-1/2 after:-translate-y-[-10px] hover:after:w-full hover:after:transition-all hover:after:duration-200"
+          href={"/"}
+        >
+          Contact
+        </Link>
+        <Link
+          className="ml-4 text-red	relative  after:content-[''] after:absolute after:bg-red after:h-[2px] after:w-0 after:top-1/2 after:left-1/2 after:transform after:-translate-x-1/2 after:-translate-y-[-10px] hover:after:w-full hover:after:transition-all hover:after:duration-200"
+          href={"/"}
+        >
+          La gazette
+        </Link>
+      </ul>
+    </nav>
+  );
+}
+
+export default NavBar;
