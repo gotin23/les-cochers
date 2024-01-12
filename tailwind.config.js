3942098888f563422ea99bea45fcefb770de0e12
Hugo Gouzy <110604228+gotin23@users.noreply.github.com>
2024-01-13 00:35:56 +0100

    feat: css

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,6 @@ module.exports = {
       white: "#f9f6f6",
       red: "#d31703",
       redHover: "#e44c3b",
-      transparent: "#FF000000",
-    },
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
     },
   },
   plugins: [],
